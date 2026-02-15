d7fbdf22e8217f531f17f3152e10b15a65091a09
fathirpokong <fathirpokong@gmail.com>
2026-02-15 09:30:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+const url = "https://script.google.com/macros/s/AKfycbwJBfxBH_MroTeO7z-96RY-voneo7awzODNjGRpqsdds-ymrnHcH0akDZFun5ZL2uc-Kg/exec";
+
+function toggleEmail() {
+    const metode = document.getElementById('metode').value;
+    document.getElementById('inputEmailBox').style.display = (metode === 'Email') ? 'block' : 'none';
+}
+
+function selectPkg(val, price, el) {
+    document.getElementById('selectedPackage').value = val;
+    document.getElementById('selectedPrice').value = price;
+    document.querySelectorAll('.package-item').forEach(i => i.classList.remove('active'));
+    el.classList.add('active');
+}
+
+function kirimData() {
+    const nama = document.getElementById('nama').value;
+    const paket = document.getElementById('selectedPackage').value;
+    const metode = document.getElementById('metode').value;
+    const email = document.getElementById('email').value || "-";
+    const btn = document.getElementById('btnOrder');
+
+    if(!nama || !paket) { alert("Isi nama dan pilih paket dulu!"); return; }
+
+    btn.disabled = true;
+    btn.innerText = "⏳ Memproses...";
+
+    fetch(url, {
+        method: "POST",
+        mode: "no-cors",
+        body: JSON.stringify({ action: "tambah", nama, paket, metode, email }),
+        headers: { "Content-Type": "text/plain" }
+    })
+    .then(() => {
+        confetti({ borderRadius: 10, particleCount: 150, spread: 70, origin: { y: 0.6 } });
+        document.querySelectorAll('.package-list, .form-container, .header p').forEach(el => el.style.display = 'none');
+        
+        const strukArea = document.getElementById('strukArea');
+        const strukContent = document.getElementById('strukContent');
+        
+        strukArea.style.display = 'block';
+        strukContent.innerHTML = `
+            ---------------------------<br>
+            ORDER ID : #MB-${Math.floor(Math.random() * 9000) + 1000}<br>
+            NAMA     : ${nama.toUpperCase()}<br>
+            PAKET    : ${paket}<br>
+            METODE   : ${metode}<br>
+            EMAIL    : ${email}<br>
+            STATUS   : MENUNGGU PEMBAYARAN<br>
+            ---------------------------
+        `;
+    });
+}
